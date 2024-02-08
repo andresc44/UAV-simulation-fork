45672e8c38687d07f1935145ca00dbbad6a419f3
JustMohsen <53262801+JustMohsen@users.noreply.github.com>
2024-02-07 23:10:18 -0500

    cleanup

--- lab2/planner.py
+++ lab2/planner.py
@@ -37,6 +37,7 @@ try:
 except ImportError:
     from .geo_controller import GeoController
 
+
 class Controller():
     """Template controller class.
 
@@ -59,7 +60,7 @@ class Controller():
         Args:
             initial_obs (ndarray): The initial observation of the quadrotor's state
                 [x, x_dot, y, y_dot, z, z_dot, phi, theta, psi, p, q, r].
-            initial_info (dict): The a priori information as a dictionary with keys
+            initial_info (dict): The priori information as a dictionary with keys
                 'symbolic_model', 'nominal_physical_parameters', 'nominal_gates_pos_and_type', etc.
             use_firmware (bool, optional): Choice between the on-board controll in `pycffirmware`
                 or simplified software-only alternative.
@@ -68,7 +69,7 @@ class Controller():
 
         """
         # [initial_info["x_reference"][0], initial_info["x_reference"][2], initial_info["x_reference"][4]]
-        self.tolerance =  initial_info["tracking_tolerance"]
+        self.tolerance = initial_info["tracking_tolerance"]
         self.radius = circle_radius
         # Save environment and control parameters.
         self.CTRL_TIMESTEP = initial_info["ctrl_timestep"]
@@ -118,8 +119,8 @@ class Controller():
             t = time[i]
             ref_x.append((radius) * math.cos(omega * t) - bias_pos[0] + init_pos[0])
             ref_y.append((radius) * math.sin(omega * t) - bias_pos[1] + init_pos[1])
-            ref_z.append( - bias_pos[2] + init_pos[2])
-        self.draw_trajectory(initial_info, np.array(ref_x), np.array(ref_y), np.array(ref_z))   
+            ref_z.append(- bias_pos[2] + init_pos[2])
+        self.draw_trajectory(initial_info, np.array(ref_x), np.array(ref_y), np.array(ref_z))
 
     def draw_trajectory(self, initial_info,
                         ref_x,
@@ -130,19 +131,19 @@ class Controller():
 
         """
 
-        step = int(ref_x.shape[0]/50)
+        step = int(ref_x.shape[0] / 50)
         for i in range(step, ref_x.shape[0], step):
-            p.addUserDebugLine(lineFromXYZ=[ref_x[i-step], ref_y[i-step], ref_z[i-step]],
-                              lineToXYZ=[ref_x[i], ref_y[i], ref_z[i]],
-                              lineColorRGB=[1, 0, 0], lineWidth=3,
-                              physicsClientId=initial_info["pyb_client"])
+            p.addUserDebugLine(lineFromXYZ=[ref_x[i - step], ref_y[i - step], ref_z[i - step]],
+                               lineToXYZ=[ref_x[i], ref_y[i], ref_z[i]],
+                               lineColorRGB=[1, 0, 0], lineWidth=3,
+                               physicsClientId=initial_info["pyb_client"])
         p.addUserDebugLine(lineFromXYZ=[ref_x[i], ref_y[i], ref_z[i]],
-                          lineToXYZ=[ref_x[-1], ref_y[-1], ref_z[-1]],
-                          lineColorRGB=[1, 0, 0], lineWidth=3,
-                          physicsClientId=initial_info["pyb_client"])
+                           lineToXYZ=[ref_x[-1], ref_y[-1], ref_z[-1]],
+                           lineColorRGB=[1, 0, 0], lineWidth=3,
+                           physicsClientId=initial_info["pyb_client"])
 
-    def computeAction(self, obs,target_p,target_v,target_a):
-      """Compute the rotor speed using the geometric controller
+    def computeAction(self, obs, target_p, target_v, target_a):
+        """Compute the rotor speed using the geometric controller
         Args:
             control_timestep (float): The time step at which control is computed.
             cur_pos (ndarray): (3,1)-shaped array of floats containing the current position.
@@ -153,24 +154,25 @@ class Controller():
             target_vel (ndarray): (3,1)-shaped array of floats containing the desired velocity.
             target_acc (ndarray): (3,1)-shaped array of floats containing the desired acceleration.
       """
-      rpms, _, _ = self.ctrl.compute_control( self.CTRL_TIMESTEP,
-                                              cur_pos=np.array([obs[0],obs[2],obs[4]]),
-                                              cur_quat=np.array(p.getQuaternionFromEuler([obs[6],obs[7],obs[8]])),
-                                              cur_vel=np.array([obs[1],obs[3],obs[5]]),
-                                              cur_ang_vel=np.array([obs[9],obs[10],obs[11]]),
-                                              target_pos=target_p,
-                                              target_vel=target_v,
-                                              target_acc=target_a
-                                              )
-      return self.KF * rpms**2
+        rpms, pos_error, _ = self.ctrl.compute_control(self.CTRL_TIMESTEP,
+                                                       cur_pos=np.array([obs[0], obs[2], obs[4]]),
+                                                       cur_quat=np.array(
+                                                           p.getQuaternionFromEuler([obs[6], obs[7], obs[8]])),
+                                                       cur_vel=np.array([obs[1], obs[3], obs[5]]),
+                                                       cur_ang_vel=np.array([obs[9], obs[10], obs[11]]),
+                                                       target_pos=target_p,
+                                                       target_vel=target_v,
+                                                       target_acc=target_a
+                                                       )
+        return self.KF * rpms ** 2, pos_error
 
     def getRef(self,
-              time,
-              obs,
-              reward=None,
-              done=None,
-              info=None
-              ):
+               time,
+               obs,
+               reward=None,
+               done=None,
+               info=None
+               ):
         """PID per-propeller thrusts with a simplified, software-only PID quadrotor controller.
 
         INSTRUCTIONS:
@@ -191,7 +193,8 @@ class Controller():
 
         """
         if self.ctrl is None:
-            raise RuntimeError("[ERROR] Attempting to use method 'getRef' but Controller was created with 'use_firmware' = True.")
+            raise RuntimeError(
+                "[ERROR] Attempting to use method 'getRef' but Controller was created with 'use_firmware' = True.")
 
         # Get the desired speed 
         self.desired_speed = self.initial_obs[3]
@@ -206,9 +209,12 @@ class Controller():
         omega = self.omega
         omega2 = omega * omega
         # Compute the reference pos, vel, and acc on the circel
-        ref_pos = np.array([self.radius * math.cos(omega * time), self.radius * math.sin(omega * time), 0.0]) - bias_pos + init_pos
-        ref_vel = np.array([-self.radius * omega * math.sin(omega * time), self.radius * omega * math.cos(omega * time), 0.0])
-        ref_acc = np.array([-self.radius * omega2 * math.cos(omega * time), -self.radius * omega2 * math.sin(omega * time), 0.0])
+        ref_pos = np.array(
+            [self.radius * math.cos(omega * time), self.radius * math.sin(omega * time), 0.0]) - bias_pos + init_pos
+        ref_vel = np.array(
+            [-self.radius * omega * math.sin(omega * time), self.radius * omega * math.cos(omega * time), 0.0])
+        ref_acc = np.array(
+            [-self.radius * omega2 * math.cos(omega * time), -self.radius * omega2 * math.sin(omega * time), 0.0])
 
         target_p = ref_pos
         target_v = ref_vel
